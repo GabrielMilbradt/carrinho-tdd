aa39339f15a87ba571c2fb29907399b77f99ef6f
GabrielMilbradt <gabrielmilbradt1313@gmail.com>
2026-09-04 14:45:59 -0300

    red: teste item 4

--- src/test/java/br/com/carrinhotdd/CarrinhoTest.java
+++ src/test/java/br/com/carrinhotdd/CarrinhoTest.java
@@ -9,19 +9,23 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class CarrinhoTest {
 
     @Test
-    void naoDeveAdicionarQuantidadeMaiorQueEstoque() {
+    void removerItemDeveReduzirOTotal()
+            throws EstoqueInsuficienteException {
 
         Carrinho carrinho = new Carrinho();
 
         Produto produto = new Produto(
                 "Camiseta",
                 50.0,
-                5
+                10
         );
 
-        assertThrows(
-                EstoqueInsuficienteException.class,
-                () -> carrinho.adicionarItem(produto, 6)
-        );
+        carrinho.adicionarItem(produto, 2);
+
+        assertEquals(100.0, carrinho.calcularTotal());
+
+        carrinho.removerItem(produto);
+
+        assertEquals(0.0, carrinho.calcularTotal());
     }
 }
